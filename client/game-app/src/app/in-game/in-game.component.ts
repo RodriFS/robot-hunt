fef96493f63d3463a08253f49094a42983e41156
Rodrigo Sanchez <rodrifs@gmail.com>
2018-09-19 14:24:20 +0200

    fix: downgrade library from phaser 3 to phaser 2 CE

--- client/game-app/src/app/in-game/in-game.component.ts
+++ client/game-app/src/app/in-game/in-game.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import Phaser from 'phaser';
+window.PIXI   = require('phaser-ce/build/custom/pixi');
+window.p2     = require('phaser-ce/build/custom/p2');
+window.Phaser = require('phaser-ce/build/custom/phaser-split');
+
 
 @Component({
   selector: 'app-in-game',
@@ -9,57 +12,58 @@ import Phaser from 'phaser';
 export class InGameComponent implements OnInit {
 
   var config = {
-      type: Phaser.AUTO,
+      type: Phaser.CANVAS,
       width: window.innerWidth,
       height: window.innerHeight,
-      physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 }
-        }
-      },
-      scene: {
+      name: 'find-me-game'
+      }
+  var scene = {
         preload: preload,
         create: create,
-        update: update
+        update: update,
+        render: render
       }
-    };
+
 
 
   var player;
-  var game = new Phaser.Game(config);
+  var cursors;
+  var game = new Phaser.Game(config.width, config.height, config.type, config.name, scene);
 
 
   function preload () {
-    this.load.setBaseURL('../assets');
-    this.load.image('person', 'person_small.png');
+    game.load.image('person', '../../assets/person_small.png');
   }
 
   function create () {
-    player = this.physics.add.image(400, 300, 'person');
-
-    player.setCollideWorldBounds(true);
+    game.world.setBounds(0,0,config.width, config.height);
+    player = game.add.sprite(0, 0, 'person');
+    game.camera.follow(player);
+    cursors = game.input.keyboard.createCursorKeys();
   }
 
   function update() {
 
-    let cursors = this.input.keyboard.createCursorKeys();
-
     if (cursors.left.isDown) {
-      player.setVelocityX(-160);
+      player.x -= 10;
 
     } else if (cursors.right.isDown) {
-      player.setVelocityX(160);
+      player.x += 10;
 
     } else if (cursors.up.isDown) {
-      player.setVelocityY(-160);
+      player.y -= 10;
 
     } else if (cursors.down.isDown) {
-      player.setVelocityY(160);
+      player.y += 10
 
-    } else {
-      player.setVelocityY(0);
-      player.setVelocityX(0);
     }
+
+    game.world.wrap(player, 0, true);
   }
+
+  function render() {
+    game.debug.cameraInfo(game.camera, 500, 32);
+    game.debug.spriteCoords(player, 32, 32);
+
+}
 }
